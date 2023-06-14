af29a8dff9ef21c01deaa7848480c2ae2c93d012
arya <134577984+Aryafore@users.noreply.github.com>
2023-06-14 09:31:59 +0700

    Update wokeh.js

--- wokeh.js
+++ wokeh.js
@@ -1,5 +1,5 @@
 // Mengatur tanggal dan waktu tujuan countdown
-var countDownDate = new Date("2023-06-14T09:30:00").getTime();
+var countDownDate = new Date("2023-06-14T09:38:00").getTime();
 
 // Memperbarui countdown setiap detik
 var x = setInterval(function () {
